ffd3ef11cfc67ef2429ac93d8bbf2b5f6dadd6e2
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-05-31 12:42:18 +0545

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookingFormComponent } from './forntend/bookingForm/booking-form/booking-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingFormComponent
   ],
   imports: [
     BrowserModule,
