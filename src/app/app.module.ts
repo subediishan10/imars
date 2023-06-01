bc3270a99317f7fff6732d9175ec55886fda5ef1
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-06-01 13:10:26 +0545

    deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookingFormComponent } from './forntend/bookingForm/booking-form/booking-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookingFormComponent
   ],
   imports: [
     BrowserModule,
